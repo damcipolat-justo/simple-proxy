0dc5a1ffa85dc0896eaa70a834cd90db5b048f92
Damian <damian@Damians-MacBook-Pro.local>
2025-05-21 12:00:27 -0300

    chore: add more services

--- app.js
+++ app.js
@@ -19,69 +19,90 @@ proxy.on('error', (err, req, res) => {
   }));
 });
 
+const searchService = (req, res) => {
+  const target = 'https://stag.justo.cloud';
+  const newPath = req.originalUrl.replace(/^\/search-service/, ''); // Quitar el prefijo
+
+  console.log(`🔄 Redirigiendo a: ${target}${newPath}`);
+
+  proxy.web(req, res, {
+    target,
+    changeOrigin: true,
+    secure: true,
+    xfwd: true,
+    headers: {
+      'X-Real-IP': req.ip,
+      'X-Forwarded-For': req.ip,
+      'X-Forwarded-Host': req.hostname,
+    },
+    selfHandleResponse: false, // Permitir que el proxy maneje la respuesta
+  });
+};
+
 const cartService = (req, res) => {
-    const target = 'https://stag.justo.cloud';
-    const newPath = req.originalUrl.replace(/^\/cart-service/, ''); // Quitar el prefijo
-  
-    console.log(`🔄 Redirigiendo a: ${target}${newPath}`);
-  
-    proxy.web(req, res, {
-      target,
-      changeOrigin: true,
-      secure: true,
-      xfwd: true,
-      headers: {
-        'X-Real-IP': req.ip,
-        'X-Forwarded-For': req.ip,
-        'X-Forwarded-Host': req.hostname,
-      },
-      selfHandleResponse: false, // Permitir que el proxy maneje la respuesta
-    });
-  };
+  const target = 'https://stag.justo.cloud';
+  const newPath = req.originalUrl.replace(/^\/cart-service/, ''); // Quitar el prefijo
+
+  console.log(`🔄 Redirigiendo a: ${target}${newPath}`);
+
+  proxy.web(req, res, {
+    target,
+    changeOrigin: true,
+    secure: true,
+    xfwd: true,
+    headers: {
+      'X-Real-IP': req.ip,
+      'X-Forwarded-For': req.ip,
+      'X-Forwarded-Host': req.hostname,
+    },
+    selfHandleResponse: false, // Permitir que el proxy maneje la respuesta
+  });
+};
 
 const orderStatusBFF = (req, res) => {
-    const target = 'http://127.0.0.1:8080';
-    const newPath = req.originalUrl.replace(/^\/order-status-bff/, ''); // Quitar el prefijo
+  const target = 'http://127.0.0.1:8080';
+  const newPath = req.originalUrl.replace(/^\/order-status-bff/, ''); // Quitar el prefijo
+
+  console.log(`🔄 Redirigiendo a: ${target}${newPath}`);
+
+  proxy.web(req, res, {
+    target,
+    changeOrigin: true,
+    secure: true,
+    xfwd: true,
+    headers: {
+      'X-Real-IP': req.ip,
+      'X-Forwarded-For': req.ip,
+      'X-Forwarded-Host': req.hostname,
+    },
+    selfHandleResponse: false, // Permitir que el proxy maneje la respuesta
+  });
+};
   
-    console.log(`🔄 Redirigiendo a: ${target}${newPath}`);
-  
-    proxy.web(req, res, {
-      target,
-      changeOrigin: true,
-      secure: true,
-      xfwd: true,
-      headers: {
-        'X-Real-IP': req.ip,
-        'X-Forwarded-For': req.ip,
-        'X-Forwarded-Host': req.hostname,
-      },
-      selfHandleResponse: false, // Permitir que el proxy maneje la respuesta
-    });
-  };
-  
-  const myAccountHelpCenter = (req, res) => {
-    const target = 'http://127.0.0.1:8081';
-    const newPath = req.originalUrl.replace(/^\/myaccount-help-center/, ''); // Ojo con el typo: "myaccont"
-  
-    console.log(`🔄 Redirigiendo a: ${target}${newPath}`);
-  
-    proxy.web(req, res, {
-      target,
-      changeOrigin: true,
-      secure: true,
-      xfwd: true,
-      headers: {
-        'X-Real-IP': req.ip,
-        'X-Forwarded-For': req.ip,
-        'X-Forwarded-Host': req.hostname,
-      },
-      selfHandleResponse: false, // Permitir que el proxy maneje la respuesta
-    });
-  };
+const myAccountHelpCenter = (req, res) => {
+  const target = 'http://127.0.0.1:8081';
+  const newPath = req.originalUrl.replace(/^\/myaccount-help-center/, ''); // Ojo con el typo: "myaccont"
+
+  console.log(`🔄 Redirigiendo a: ${target}${newPath}`);
+
+  proxy.web(req, res, {
+    target,
+    changeOrigin: true,
+    secure: true,
+    xfwd: true,
+    headers: {
+      'X-Real-IP': req.ip,
+      'X-Forwarded-For': req.ip,
+      'X-Forwarded-Host': req.hostname,
+    },
+    selfHandleResponse: false, // Permitir que el proxy maneje la respuesta
+  });
+};
 
 router.use('/order-status-bff/', orderStatusBFF);
 router.use('/myaccount-help-center/', myAccountHelpCenter);
-router.use('/cartService/', cartService);
+router.use('/search-service/', searchService);
+router.use('/cart-service/', cartService);
 app.use(cors());
 app.use(router);
 
